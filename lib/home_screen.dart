cadcd5c2dbf4de9b0e872a30dff7f8c8ae693b02
shamim2810 <shamim149120@gmail.com>
2024-02-10 22:38:33 +0600

    Counter App

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,80 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_grid/responsive_grid.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int counterApp = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Responsive Grid'),
-          centerTitle: true,
-          backgroundColor: Colors.grey,
-        ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: ResponsiveGridRow(
-              children: [
-                ResponsiveGridCol(
-                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
-                    child: Container(
-                      color: Colors.deepOrange,
-                      height: 100,
-                    ),
-                ),
-                ResponsiveGridCol(
-                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
-                  child: Container(
-                    color: Colors.pink,
-                    height: 100,
-                  ),
-                ),
-                ResponsiveGridCol(
-                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
-                  child: Container(
-                    color: Colors.grey,
-                    height: 100,
-                  ),
-                ),
-                ResponsiveGridCol(
-                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
-                  child: Container(
-                    color: Colors.red,
-                    height: 100,
-                  ),
-                ),
-                ResponsiveGridCol(
-                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
-                  child: Container(
-                    color: Colors.brown,
-                    height: 100,
-                  ),
-                ),
-                ResponsiveGridCol(
-                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
-                  child: Container(
-                    color: Colors.blue,
-                    height: 100,
-                  ),
-                ),
-                ResponsiveGridCol(
-                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
-                  child: Container(
-                    color: Colors.purple,
-                    height: 100,
-                  ),
-                ),
-                ResponsiveGridCol(
-                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
-                  child: Container(
-                    color: Colors.yellow,
-                    height: 100,
-                  ),
-                ),
-              ]
-          ),
-        ),
+      appBar: AppBar(
+        title: const Text('Counter App',style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        backgroundColor: Colors.grey,
+      ),
+      body: Center(
+        child: Text(counterApp.toString()),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey,
+        onPressed: () {
+          setState(() {
+            counterApp = counterApp+1;
+          });
+        },
+        child: Icon(Icons.add,),
+      ),
     );
   }
 }
