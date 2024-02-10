fcf93093c43ea4f9e8be09df047fbf41b8f707e6
shamim2810 <shamim149120@gmail.com>
2024-02-10 21:09:25 +0600

    Responsive Grid

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rabbil/style.dart';
+import 'package:responsive_grid/responsive_grid.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -8,25 +8,73 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Media Query'),
+          title: const Text('Responsive Grid'),
           centerTitle: true,
           backgroundColor: Colors.grey,
         ),
-
-        //Row and Column 2 ta diyei check korte hobe
-        // Expanded and Flexiable semilar
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(
-                child: Text(
-                  'Hello Flutter',
-                  style: HeadLine(context),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: ResponsiveGridRow(
+              children: [
+                ResponsiveGridCol(
+                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
+                    child: Container(
+                      color: Colors.deepOrange,
+                      height: 100,
+                    ),
                 ),
-              ),
-            ],
+                ResponsiveGridCol(
+                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
+                  child: Container(
+                    color: Colors.pink,
+                    height: 100,
+                  ),
+                ),
+                ResponsiveGridCol(
+                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
+                  child: Container(
+                    color: Colors.grey,
+                    height: 100,
+                  ),
+                ),
+                ResponsiveGridCol(
+                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
+                  child: Container(
+                    color: Colors.red,
+                    height: 100,
+                  ),
+                ),
+                ResponsiveGridCol(
+                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
+                  child: Container(
+                    color: Colors.brown,
+                    height: 100,
+                  ),
+                ),
+                ResponsiveGridCol(
+                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
+                  child: Container(
+                    color: Colors.blue,
+                    height: 100,
+                  ),
+                ),
+                ResponsiveGridCol(
+                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
+                  child: Container(
+                    color: Colors.purple,
+                    height: 100,
+                  ),
+                ),
+                ResponsiveGridCol(
+                  xl: 3, lg: 3, md: 4,sm: 6, xs: 12,
+                  child: Container(
+                    color: Colors.yellow,
+                    height: 100,
+                  ),
+                ),
+              ]
           ),
-        ));
+        ),
+    );
   }
 }
